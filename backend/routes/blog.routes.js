da5d7dd63d57fb20f4f5447a3f886431aa480de1
I750248 <dan.miuta.24@lehre.mosbach.dhbw.de>
2025-06-13 21:50:03 +0200

    delete blog router update

--- backend/routes/blog.routes.js
+++ backend/routes/blog.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth/authMiddleware');
-const {createBlog, fetchBlogsByUser , fetchBlogBySlug} = require('../controllers/blog.controller');
+const {createBlog, fetchBlogsByUser , fetchBlogBySlug,deleteBlog} = require('../controllers/blog.controller');
 const {createBlogRules,validateBlog} = require('../middleware/validators/blogValidators');
 const {validateBlogCoverImage,validateBlogContentImages}= require('../middleware/imageUpload')
 router.post('/create',auth,validateBlogCoverImage(),validateBlogContentImages,createBlogRules,validateBlog,createBlog);
 router.get('/my-blogs', auth, fetchBlogsByUser);
 router.get('/get-blog/:slug', fetchBlogBySlug);
+router.delete('/delete/:id', auth, deleteBlog);
 
 module.exports = router
